ef8aa3bc801c69020a5448140fd594f3cadd6cf7
“matcs06” <you@example.com>
2023-01-10 18:10:55 -0300

    enabling duration field on serviceupdate

--- src/pages/admin/app/updateService/index.tsx
+++ src/pages/admin/app/updateService/index.tsx
@@ -11,59 +11,59 @@ function UpdateService(props: any) {
    const [serviceInfo, setServiceInfo] = useState()
    const [value, setValue] = useState()
    const [duration, setDuration] = useState()
-   const enabledProps:boolean = props.router.query.isEnabled == 'false' ? false : true 
+   const enabledProps: boolean = props.router.query.isEnabled == 'false' ? false : true
    const [isEnabled, setIsEnabled] = useState(enabledProps)
 
-   const handleUpdate = async ()=>{
+   const handleUpdate = async () => {
       const token = localStorage.getItem("token");
       var existentServiceName = ''
       var existentDuration = ''
       var existentDescription = ''
       var existentValue = ''
-      
-      if(!serviceName){
+
+      if (!serviceName) {
          existentServiceName = props.router.query.serviceName;
-      }else{
+      } else {
          existentServiceName = serviceName
       }
 
-      if(!duration){
+      if (!duration) {
          existentDuration = props.router.query.serviceDuration;
-      }else{
+      } else {
          existentDuration = duration
       }
 
-      if(!serviceInfo){
+      if (!serviceInfo) {
          existentDescription = props.router.query.serviceDescription;
-      }else{
+      } else {
          existentDescription = serviceInfo
       }
 
-      if(!value){
+      if (!value) {
          existentValue = props.router.query.serviceValue;
-      }else{
+      } else {
          existentValue = value
       }
 
       try {
          await api.patch(
-         `/products/${props.router.query.serviceId}`,
-         {
-            name: existentServiceName,
-            description: serviceInfo,
-            price: value,
-            duration: existentDuration,
-            enabled: isEnabled
-         },
-         {
-            headers: {
-               Authorization: "Bearer " + token,
+            `/products/${props.router.query.serviceId}`,
+            {
+               name: existentServiceName,
+               description: serviceInfo,
+               price: value,
+               duration: existentDuration,
+               enabled: isEnabled
             },
-         }
+            {
+               headers: {
+                  Authorization: "Bearer " + token,
+               },
+            }
          );
 
          window.alert(
-         `Serviço atualizado com sucesso`
+            `Serviço atualizado com sucesso`
          );
 
       } catch (error) {
@@ -72,40 +72,40 @@ function UpdateService(props: any) {
 
    }
 
-   const handleBack = ()=>{
+   const handleBack = () => {
       window.location.pathname = "/admin/app/"
    }
 
-   const controlEnabled = ()=>{
+   const controlEnabled = () => {
       setIsEnabled(!isEnabled)
    }
 
 
-   return(
+   return (
       <div className={styles.container}>
-          <div className={styles.panelContainer}>
-          <div className={styles.panel}>
-            <div className={styles.serviceName}>
-                 <Input type="text" placeholder={props.router.query.serviceName} name="Servico" setfieldvalue={setServiceName}/>
-             </div>
-             <div className={styles.serviceDescription}>
-                 <Input type="text" placeholder="Descrição do serviço" name="Descricao" setfieldvalue={setServiceInfo}/>
-             </div>
-             <div className={styles.valueAndDuration}>
-                <Input type="text" placeholder="Valor" name="Valor" setfieldvalue={setValue}/>
-                <Input disabled="disabled" type="time" placeholder="Duração" name="Duração" setfieldvalue={setDuration}/>
-             </div>
-             <div className={styles.enabledStyle}>
+         <div className={styles.panelContainer}>
+            <div className={styles.panel}>
+               <div className={styles.serviceName}>
+                  <Input type="text" placeholder={props.router.query.serviceName} name="Servico" setfieldvalue={setServiceName} />
+               </div>
+               <div className={styles.serviceDescription}>
+                  <Input type="text" placeholder={props.router.query.serviceDescription} name="Descricao" setfieldvalue={setServiceInfo} />
+               </div>
+               <div className={styles.valueAndDuration}>
+                  <Input type="text" placeholder={props.router.query.serviceValue} name="Valor" setfieldvalue={setValue} />
+                  <Input type="time" placeholder="Duração" name="Duração" setfieldvalue={setDuration} />
+               </div>
+               <div className={styles.enabledStyle}>
                   <label className={styles.label} htmlFor="payment_status">Habilitar</label>
 
-                 <input className="checkBox" type="checkbox" id="payment_status" checked={isEnabled} onChange={controlEnabled} />
+                  <input className="checkBox" type="checkbox" id="payment_status" checked={isEnabled} onChange={controlEnabled} />
                </div>
-             <div className={styles.buttonContainer}>
-                <Button page="/admin/app/" handleClick={handleUpdate} >Atualizar</Button>
-                <Button page="/admin/app/" handleClick={handleBack} >Voltar</Button>
-             </div>
-              
-         </div>       
+               <div className={styles.buttonContainer}>
+                  <Button page="/admin/app/" handleClick={handleUpdate} >Atualizar</Button>
+                  <Button page="/admin/app/" handleClick={handleBack} >Voltar</Button>
+               </div>
+
+            </div>
          </div>
       </div>
    )
