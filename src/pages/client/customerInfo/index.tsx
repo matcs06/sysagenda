1baaae3ec4f9cce0ae089bc912aba100b703f004
“matcs06” <you@example.com>
2023-02-07 21:41:58 -0300

    fixing placeholder

--- src/pages/client/customerInfo/index.tsx
+++ src/pages/client/customerInfo/index.tsx
@@ -5,12 +5,12 @@ import Input from "../../../components/input"
 import PhoneInput from "../../../components/PhoneInput"
 import styles from "./customerInfo.module.scss"
 
-function CustomerInfo(props:any){
+function CustomerInfo(props: any) {
 
     const [customerName, setCustomerName] = useState("")
     const [customerNumber, setCustomerNumber] = useState("")
 
-    const handleClick = async()=>{
+    const handleClick = async () => {
 
         const serviceDuration = props.router.query.serviceDuration;
         const choosedDate = props.router.query.choosedDate;
@@ -20,15 +20,15 @@ function CustomerInfo(props:any){
 
         let newServiceDuration: string;
         let newChoosedTime: string;
-        if(serviceDuration.split(":").length != 3){
+        if (serviceDuration.split(":").length != 3) {
             newServiceDuration = serviceDuration + ":00"
-        }else{
+        } else {
             newServiceDuration = serviceDuration
         }
 
-        if(choosedTime.split(":").length != 3){
+        if (choosedTime.split(":").length != 3) {
             newChoosedTime = choosedTime + ":00"
-        }else{
+        } else {
             newChoosedTime = choosedTime
         }
 
@@ -36,15 +36,15 @@ function CustomerInfo(props:any){
 
         try {
 
-            if(!customerName){
+            if (!customerName) {
                 throw new Error("Informe o seu nome para finalizar o agendamento")
             }
 
-            if(!customerNumber || customerNumber.length < 8){
+            if (!customerNumber || customerNumber.length < 8) {
                 throw new Error("Informe um número de telefone válido com o DD: EX: 98 991414243")
             }
 
-            await api.post('/schedules',{
+            await api.post('/schedules', {
                 customer_name: customerName,
                 service: serviceName,
                 date: choosedDate,
@@ -57,40 +57,40 @@ function CustomerInfo(props:any){
 
             Router.push({
                 pathname: '/client/finalScreen',
-                query:{valor:props.router.query.servicePrice, serviceName, serviceTime: choosedTime, customerName }
+                query: { valor: props.router.query.servicePrice, serviceName, serviceTime: choosedTime, customerName }
             })
 
         } catch (error) {
-            if(error.message.indexOf("Informe") > -1){
+            if (error.message.indexOf("Informe") > -1) {
                 window.alert(error)
-            }else{
+            } else {
                 window.alert("Erro ao criar agendamento, tente novamente e confirme se já não existe um agendamento feito para esse horário!")
             }
         }
-      
+
     }
 
-    return(
-       <div className={styles.container}>
-           <div>
-               <h2 className={styles.screenTitle}>Informações</h2>
-           </div>
-           <div className={styles.inputContainer}>
-               <p>Nome</p>
-               <Input type="text" placeholder=""  name="name" setfieldvalue={setCustomerName} autocomplete="off" />
-               <p>Número de telegone (WhatsApp)</p>
-               <PhoneInput type="text" placeholder=""  name="number" autocomplete="off" setfieldvalue={setCustomerNumber}/>
-           </div>
+    return (
+        <div className={styles.container}>
+            <div>
+                <h2 className={styles.screenTitle}>Informações</h2>
+            </div>
+            <div className={styles.inputContainer}>
+                <p>Nome</p>
+                <Input type="text" placeholder="" name="name" setfieldvalue={setCustomerName} autocomplete="off" />
+                <p>Número de telefone (WhatsApp)</p>
+                <PhoneInput type="text" placeholder="" name="number" autocomplete="off" setfieldvalue={setCustomerNumber} />
+            </div>
 
-           <div className={styles.paymentInfo}>
-               <h2  className={styles.value}>Valor: R$ {props.router.query.servicePrice}</h2>
-               <h2  className={styles.payment}>Pagamento é feito no momento do serviço</h2>
-           </div>
+            <div className={styles.paymentInfo}>
+                <h2 className={styles.value}>Valor: R$ {props.router.query.servicePrice}</h2>
+                <h2 className={styles.payment}>Pagamento é feito no momento do serviço</h2>
+            </div>
 
-           <div className={styles.buttonContainer} onClick={handleClick}>
+            <div className={styles.buttonContainer} onClick={handleClick}>
                 <button>Agendar</button>
             </div>
-       </div>    
+        </div>
     )
 
 
